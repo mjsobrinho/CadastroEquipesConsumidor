e1eaef9e57c0221fbd03eba41a8043b873252393
mjsobrinho <mjsobrinho@gmail.com>
2024-09-28 11:28:02 -0300

    Descrição clara das alterações realizadas

--- Controllers/EquipesPessoasController.cs
+++ Controllers/EquipesPessoasController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CadastroEquipesConsumidor.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CadastroEquipesConsumidor.Controllers
 {
     public class EquipesPessoasController : Controller
     {
-        public IActionResult Index()
+
+        private readonly HttpClient _httpClient;
+
+        // Aqui deve ser atualizado o endereço da API
+        string apiEquipes = "https://localhost:7088/api/EquipesPessoas";
+
+        public EquipesPessoasController(HttpClient httpClient)
         {
-            return View("EquipesPessoas"); // Retorna a view EquipePessoas.cshtml
+            _httpClient = httpClient;
+        }
+
+        // Exibe a lista de equipes com pessoas
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var equipes = await _httpClient.GetFromJsonAsync<List<EquipesPessoasModel>>(apiEquipes);
+                if (equipes == null)
+                {
+                    return View("Error", "Não foi possível obter as pessoas.");
+                }
+
+                return View(equipes); // Passa a lista de equipes para a view
+            }
+            catch (HttpRequestException e)
+            {
+                return View("Error", $"Erro ao acessar a API: {e.Message}");
+
+            }
+            catch (Exception e)
+            {
+                return View("Error", $"Ocorreu um erro inesperado: {e.Message}");
+            }
         }
     }
 }
---
+++ Models/EquipesPessoasModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroEquipesConsumidor.Models
+{
+    public class EquipesPessoasModel
+    {
+
+        public Guid? Id { get; set; } // Chave primária do tipo GUID
+
+        public string Nm_Equipe { get; set; } // Nome da equipe
+        public string Nome { get; set; } // Nome da equipe
+
+        public int idade { get; set; } // Idade mínima
+
+        public string Sexo { get; set; } // Sexo
+        public string Cpf { get; set; } // Cpf
+    }
+}
